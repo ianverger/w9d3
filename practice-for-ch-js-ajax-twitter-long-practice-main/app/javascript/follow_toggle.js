95ece9b765a568c47fd3e56eb1c0cc4da35c0acc
ianverger <ian.verger@gmail.com>
2022-09-14 17:37:18 -0400

    final

--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
@@ -2,24 +2,46 @@ import { API, broadcast } from "./util";
 
 export default class FollowToggle {
   constructor(toggleButton) {
-    // Your code here
+    this.toggleButton = toggleButton;
+    this.toggleButton.addEventListener("click", this.handleClick.bind(this));
+    this.temp = "testing1234";
   }
 
   async handleClick(event) {
-    // Your code here
+    event.preventDefault()
+    if (this.followState === "followed") this.follow.bind(this)();
+    if (this.followState === "unfollowed") this.unfollow.bind(this)();
   }
 
   async follow() {
-    // Your code here
+    await API.followUser(this.toggleButton.dataset.userId)
   }
 
   async unfollow() {
-    // Your code here
+    await API.unfollowUser(this.toggleButton.dataset.userId)
   }
 
   render() {
     switch (this.followState) {
-      // Your code here
+      case "followed":
+        this.disabled = false;
+        this.toggleButton.innerText = "Unfollow!"
+        break;
+      case "unfollowed":
+        this.disabled = false;
+        this.toggleButton.innerText = "Follow!!"
+        break;
+      // case "following":
+      //   this.disabled = true;
+      //   this.toggleButton.innerText = "Following"
+      //   break;
+      // case "unfollowed":
+      //   this.disabled = true;
+      //   this.toggleButton.innerText = "Unfollowing!!"
+      //   break;
+      default:
+        console.log("oops something went wrong");
+        break;
     }
   }
 
--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/util/api.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/util/api.js
@@ -2,9 +2,23 @@ const csrfToken = document.querySelector("meta[name=csrf-token]").content;
 
 async function customFetch(url, options = {}) {
   options.headers = {
-    // Your code here
+    "X-CSRF-Token": csrfToken,
+    Accept: 'application/json',
     ...options.headers
   };
+  
+  const response =  await fetch(url, options)
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw response;
+  }
+}
 
-  return await fetch(url, options);
+export function followUser(id) {
+    return customFetch(`/users/${id}/follow`, {method: 'POST'});
+}
+
+export function unfollowUser(id) {
+  return customFetch(`/users/${id}/follow`, {method: 'DELETE'});
 }
